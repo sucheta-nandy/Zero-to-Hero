ec4153e093c50c4d37c965fce41af6bf06b23f10
Sahil-Shende <47074843+Sahil-Shende@users.noreply.github.com>
2019-10-12 11:23:04 +0530

    Update FizzBuzz.cpp (#154)

--- GCC/FizzBuzz.cpp
+++ GCC/FizzBuzz.cpp
@@ -11,15 +11,15 @@ int main(){
   
   
   for(int i = 1; i <=100; i++){
-
+//check if number is divisible by both 3 and 5
     if(i % 3 == 0 && i % 5 == 0){
       std::cout << "FizzBuzz\n";
     }
     else if(i % 3 == 0){
-      std::cout << "Fizz\n";
+      std::cout << "Fizz\n";//check if number is divisble by 3
     }
     else if(i % 5 == 0){
-      std::cout << "Buzz\n";
+      std::cout << "Buzz\n";//check if number is divisble by 5
     }
     else{
       std::cout << i << "\n";
